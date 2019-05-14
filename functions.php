301f405f6e6cb497077ac937d5ecd049b46177c1
maria <debawimaria.fsm@gmail.com>
2019-05-14 23:18:31 +0100

    06

--- functions.php
+++ functions.php
@@ -3,6 +3,7 @@
  require get_template_directory() . '/inc/enqueue.php' ;
  require get_template_directory() . '/inc/sunset_theme_support.php' ;
  require get_template_directory() . '/inc/custom_post_type.php' ;
+ require get_template_directory() . '/inc/cleanup.php' ;
 
 
 
--- header.php
+++ header.php
@@ -4,17 +4,21 @@
      @package theme sunset
  ***************************/
 ?>
+
 <?php ?>
 <!DOCTYPE html>
-<html lang="en">
+<html <?php language_attributes(); ?>>
 
 <head>
     <meta charset="<?php bloginfo('charset') ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="<?php bloginfo('description'); ?>">
     <link rel="profile" href="http://gmpg.org/xfn/11">
     <?php if (is_singular() && pings_open(get_queried_object())) : ?>
         <link rel="pingback" href="<?php bloginfo('bingback_url'); ?>">
     <?php endif; ?>
+    <title><?php bloginfo('name');
+            wp_title(); ?></title>
     <?php wp_head(); ?>
 </head>
 
@@ -23,9 +27,9 @@
         <div class="row">
             <div class="col-xs-12">
                 <div class="header_container background_image text-center" style="background-image:url(<?php header_image(); ?>); ">
-                    <div class="header-content table " >
+                    <div class="header-content table ">
                         <div class="table-cell">
-                            <h1 class="site-title icon"> 
+                            <h1 class="site-title icon">
                                 <span class="sunset-logo"></span>
                                 <span class="hide"> <?php bloginfo('name'); ?></span>
                             </h1>

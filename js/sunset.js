95ce04ed9cf458a62d7ef1d34be4b3c3a9a6d420
maria <debawimaria.fsm@gmail.com>
2019-05-22 04:31:38 +0100

    fix-b 2

--- js/sunset.js
+++ js/sunset.js
@@ -3,33 +3,7 @@
 /* ===============================
     Add a varible to a div 
  ================================== */
-/*
-$(function () {
-    /* when the document is ready  */
-/*    sunset_get_thumbn('.sunset-carousel-thumb');
 
-});
-
-$('.sunset-carousel-thumb').on('slid.bs.carousel' , function(){ 
-    sunset_get_thumbn('.sunset-carousel-thumb');
-});
-
-function sunset_get_thumbn(carousel){
-    $(carousel).each(function(){
-        var nextThumb = $('.item.active').find('.next-image-preview').data('image');
-       var prevThumb = $('.item.active').find('.prev-image-preview').data('image');
-
-        //console.log(nextThumb);
-        $(this).find('.right.carousel-control').find('.thumbnail-container').css({
-            "background-image": "url(" + nextThumb + ")"
-        });
-        $(this).find('.left.carousel-control').find('.thumbnail-container').css({
-            "background-image": "url(" + prevThumb + ")"
-    });
-    });
-   
-}
-*/
 jQuery(document).ready(function ($) {
     //custom Sunset scripts
 

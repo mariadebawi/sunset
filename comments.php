22dd95ae07ec9a6749d8cb6eed78977a3435956e
maria <debawimaria.fsm@gmail.com>
2019-05-31 11:07:33 +0100

    comment02

--- comments.php
+++ comments.php
@@ -17,6 +17,37 @@ if (post_password_required()) {
 
     <?php if (have_comments()) : ?>
 
+        <h2 class="comment-title">
+            <?php
+            //2 comments on “Gallery Post Format”
+            printf(
+                esc_html(_nx('One comment on &ldquo;%2$s&rdquo;', '%1$s comments on &ldquo;%2$s&rdquo;', get_comments_number(), 'comments title', 'Sunset')),
+                number_format_i18n(get_comments_number()),
+                '<span>' . get_the_title() . '</span>'
+            );
+            ?>
+        </h2>
+
+        <?php
+        if (get_comment_pages_count() > 1 && get_option('page_comments')) : ?>
+            <nav id="comment-nav-top" class="comment-navigation" role="navigation">
+                <div class="row">
+                    <div class="col-xs-12 col-sm-6">
+                        <div class="post-link-nav">
+                            <span class="sunset-icon sunset-chevron-left" aria-hidden="true"></span>
+                            <?php previous_comments_link(esc_html__('Older comments', 'Sunset')) ?>
+                        </div>
+                    </div>
+                    <div class="col-xs-12 col-sm-6 text-right">
+                        <div class="post-link-nav">
+                            <span class="sunset-icon sunset-chevron-right" aria-hidden="true"></span>
+                            <?php next_comments_link(esc_html__('Newer comments', 'Sunset')) ?>
+                        </div>
+                    </div>
+                </div>
+            </nav>
+        <?php endif; ?>
+
         <ol class="comment-list">
             <?php
             $args = array(
@@ -28,7 +59,7 @@ if (post_password_required()) {
                 'replay_text'  => 'Replay',
                 'page'         => '',
                 'per_page'     => '',
-                'avatar_size'  => 32,
+                'avatar_size'  => 64,
                 'reverse_top_level' => '',
                 'format'       => 'html5',
                 'short_ping'   => false,
@@ -38,12 +69,33 @@ if (post_password_required()) {
             ?>
         </ol>
 
+        <?php
+        if (get_comment_pages_count() > 1 && get_option('page_comments')) : ?>
+            <nav id="comment-nav-bottom" class="comment-navigation" role="navigation">
+                <div class="row">
+                    <div class="col-xs-12 col-sm-6">
+                        <div class="post-link-nav">
+                            <span class="sunset-icon sunset-chevron-left" aria-hidden="true"></span>
+                            <?php previous_comments_link(esc_html__('Older comments', 'Sunset')) ?>
+                        </div>
+                    </div>
+                    <div class="col-xs-12 col-sm-6 text-right">
+                        <div class="post-link-nav">
+                            <span class="sunset-icon sunset-chevron-right" aria-hidden="true"></span>
+                            <?php next_comments_link(esc_html__('Newer comments', 'Sunset')) ?>
+                        </div>
+                    </div>
+                </div>
+            </nav>
+        <?php endif; ?>
+
+
         <?php if (!(comments_open() && get_comments_number())) : ?>
             <p class="no-comments"> <?php esc_html_e('Comments are closed', 'Sunset'); ?> </p>
         <?php endif; ?>
 
     <?php endif; ?>
-    
+
     <?php comment_form(); ?>
 
 </div>
